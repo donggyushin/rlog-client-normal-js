203c3728b837d18c052f7f3713587a53bf562168
shin donggyu <donggyu9410@gmail.com>
2019-09-30 20:08:21 +0900

    push before deployment

---
+++ build/precache-manifest.107724d70eb8c34487343e26bea4cc42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef54a02dd3de3468a5f7ac584a5384e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "377fac8f268c17884f13",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "73abd6af0432085aecb5",
+    "url": "/static/js/2.0a7aba33.chunk.js"
+  },
+  {
+    "revision": "377fac8f268c17884f13",
+    "url": "/static/js/main.2ef1ad27.chunk.js"
+  },
+  {
+    "revision": "757122ab4549f6cde604",
+    "url": "/static/js/runtime-main.3f89ebfe.js"
+  },
+  {
+    "revision": "38372de13d0dd2a9b9e1c99f3f56376a",
+    "url": "/static/media/MyDoubleLucky.38372de1.jpeg"
+  }
+]);
